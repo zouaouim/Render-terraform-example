8d809bf71e0c190bab1ea44f2bbae39be08a43bd
ZOUAOUI Malak <malak.zouaoui@forvia.com>
2025-12-18 10:26:14 +0100

    attempt3

--- main.tf
+++ main.tf
@@ -17,11 +17,7 @@ resource "render_static_site" "example" {
   branch        = "main"
   build_command = "" # no build needed for html file, ---- ken 3ana container we prepare Dockerfile
   publish_path  = "."
-
-
   auto_deploy    = true
-
-
 }
 
 output "site_url" {
